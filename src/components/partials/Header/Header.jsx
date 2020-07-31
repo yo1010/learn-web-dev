8a91e80e352e36cfcf5f6a03ab309e89db04b55f
Yavor Dimitrov <yavor.v.dimitrov@gmail.com>
2020-07-31 19:49:36 +0300

    style changes to home page

--- src/components/partials/Header/Header.jsx
+++ src/components/partials/Header/Header.jsx
@@ -6,7 +6,7 @@ import './styles/Header.scss';
 const Header = () => {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-fixed-top">
             <Link class="navbar-brand" to="#">Tech<span class="green">Topia</span></Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@ const Header = () => {
                 </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <input class="form-control mr-sm-2 nav-input" type="search" placeholder="Search" aria-label="Search" />
                     <button class="btn my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
